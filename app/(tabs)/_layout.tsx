8bcd65a51e41e973526666bba3543b014d8e818d
baptisteMahe <baptiste.mahe@unicofrance.com>
2025-06-20 11:11:40 +0200

    Starter

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,16 +27,16 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="friends"
         options={{
-          title: 'Explore',
+          title: 'Friends',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
